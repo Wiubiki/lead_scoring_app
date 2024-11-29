2664399f6a16d270ad70c6c3f8e09758c9ca06c3
Wiubiki <aristeidis.kypriotis@gmail.com>
2024-11-29 15:48:32 +0200

    Minor fixes to app due to authentication functions

--- src/auth_library.py
+++ src/auth_library.py
@@ -2,10 +2,10 @@ import bcrypt
 import streamlit as st
 
 credentials = {
-    "admin": b"$2b$12$stKOH.ZarvtSMIhzd9miVuYExQYmrzqyP7TB0y3BRbkihAwLXuC.y",  # Hash for "mypassword123"
+    "admin": b"$2b$12$stKOH.ZarvtSMIhzd9miVuYExQYmrzqyP7TB0y3BRbkihAwLXuC.y",  # "bouboulis"
     "lids": b"$2b$12$n5cdt2fT6.Az6k.6r4UaBef81NCPEwf9hp6KNFHvHv.fEutchfapK",
     "letrim": b"$2b$12$i/9kpIpakVxJN9v5EpJcdejG9DdWaQid7IG5gzODPRCN5G47qk8Z2",
-    "guest": b"$2b$12$/Opf7JIp7qvgI0SNcVVA/uTFIxMRDJpH5sJ/uxi/k6B92XgN19u.e"  # Replace with another hash
+    "guest": b"$2b$12$/Opf7JIp7qvgI0SNcVVA/uTFIxMRDJpH5sJ/uxi/k6B92XgN19u.e"  # User_anonym#$
 }
 
 def authenticate(username, password):
